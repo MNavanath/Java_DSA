cc696ed2806613ff50ea169e0fa983e74e1c0d8d
MNavanath <madakattenavanath@gmail.com>
2025-01-18 23:23:40 +0530

    Add

---
+++ Graph_DSA/practice.java
@@ -0,0 +1,16 @@
+package Graph_DSA;
+import java.util.*;
+public class practice{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        message();
+    }
+     static void message(){
+        System.out.println("Hello World1");
+        message2();
+     }
+     static void message2(){
+        System.out.println("Hello World2");
+     }
+
+}

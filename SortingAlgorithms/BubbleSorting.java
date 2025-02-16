5e4b8a1d270641bfecc26ca733e9aa409957af86
MNavanath <madakattenavanath@gmail.com>
2025-02-17 01:16:03 +0530

    Add or Update

--- SortingAlgorithms/BubbleSorting.java
+++ SortingAlgorithms/BubbleSorting.java
@@ -1,7 +1,29 @@
 package SortingAlgorithms;
 
+import java.util.Arrays;
+
 public class BubbleSorting {
     public static void main(String[] args) {
+        int[] arr = {1,2,4,2,5,6,7,2};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    static void bubbleSort(int[] arr){
+        boolean swaped;
+        for(int i =0; i < arr.length;i++){
+            swaped = false;
+            for(int j = 1; j < arr.length-i;j++){
+                if(arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swaped = true;
+                }
+            }
+            if(!swaped){
+                break;
+            }
+        }
     }
 }

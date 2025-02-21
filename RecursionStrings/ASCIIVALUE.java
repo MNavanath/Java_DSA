ad200f05386c4732956670c4a96d1a8a84d85bad
MNavanath <madakattenavanath@gmail.com>
2025-02-22 03:42:21 +0530

    Subsequences and subsets

---
+++ RecursionStrings/ASCIIVALUE.java
@@ -0,0 +1,11 @@
+package RecursionStrings;
+
+public class ASCIIVALUE {
+    public static void main(String[] args) {
+        char ch = 'a';
+        System.out.println(ch+0);
+        System.out.println((char)(ch+1));
+    }
+
+
+}

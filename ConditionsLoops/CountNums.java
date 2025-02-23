0b5d522a80f29ceb821da478033394fd005d8243
MNavanath <madakattenavanath@gmail.com>
2025-02-23 19:29:38 +0530

    Conditions

--- ConditionsLoops/CountNums.java
+++ ConditionsLoops/CountNums.java
@@ -1,4 +1,18 @@
 package ConditionsLoops;
 
 public class CountNums {
+    public static void main(String[] args) {
+        int num = 5334;
+
+        int count = 0;
+
+        while(num>0){
+            int rem = num % 10;
+            if(rem == 5){
+                count++;
+            }
+            num = num/10;
+        }
+        System.out.println(count);
+    }
 }
--- ConditionsLoops/ReverseNumber.java
+++ ConditionsLoops/ReverseNumber.java
@@ -1,4 +1,26 @@
 package ConditionsLoops;
 
+import java.util.Scanner;
+
 public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int original = num;
+        int sum = 0;
+
+        while(num > 0){
+            int rem = num % 10;
+            sum = sum * 10 + rem ;
+            num = num /10;
+
+        }
+
+        System.out.println(sum);
+        if (sum == original){
+            System.out.println("It is Pallindrome");
+        }else{
+            System.out.println("Not a pallindrome");
+        }
+    }
 }
